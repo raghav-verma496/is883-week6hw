ad5b2a09cb445be314f3121e1afd10daedcd2bd2
raghav-verma496 <156374445+raghav-verma496@users.noreply.github.com>
2024-10-20 18:32:02 -0400

    Update streamlit_app.py from professor's repo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,25 @@
 import streamlit as st
+from openai import OpenAI
+import os
 
-st.title("🎈 My new app")
+st.title("My Super Awesome OpenAI API Deployment!")
+
+prompt = st.text_input("What is your prompt today?", "Damascus is")
+
+### Load your API Key
+os.environ["OPENAI_API_KEY"] = st.secrets["OpenAIkey"]
+
+### OpenAI stuff
+client = OpenAI()
+response = client.chat.completions.create(
+  model="gpt-4o-mini",
+  messages=[
+    {"role": "system", "content": "Complete the following prefix"},
+    {"role": "user", "content": prompt}
+  ],
+)
+
+### Display
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    response.choices[0].message.content
 )
